a2b4e5f058adfd6219f66f059f3af8529a19b915
cybector <tylersphotography2018@gmail.com>
2023-06-01 22:40:47 -0400

    updated main.py

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ from recipe_api.recipe_api_handler import RecipeAPIHandler
 
 
 def main():
+    # Load the Spoonacular API key from the .env file
     load_dotenv()
     api_key = os.getenv("SPOONACULAR_API_KEY")
     if not api_key:
@@ -15,17 +16,24 @@ def main():
         )
         return
 
+    # Initialize the database handler
     db_handler = DBHandler("ingredients.db")
+
+    # Initialize the recipe API handler
     recipe_handler = RecipeAPIHandler(api_key)
 
+    # Add some ingredients to the database
     db_handler.add_ingredient("Milk", "2023-06-10")
     db_handler.add_ingredient("Eggs", "2023-06-15")
 
+    # Get all ingredients from the database
     ingredients = db_handler.get_ingredients()
 
+    # Fetch recipes from the API
     recipes = recipe_handler.fetch_recipes(ingredients)
     print(recipes)
 
+    # Close the database connection
     db_handler.close()
 
 
