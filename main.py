0185394bc33836a24af19c494d8f196c24b1dc89
cybector <tylersphotography2018@gmail.com>
2023-06-04 08:02:40 -0400

    Refactored main.py for better readability and added inline comments for clarity.

--- main.py
+++ main.py
@@ -1,33 +1,44 @@
 import os
-
 from dotenv import load_dotenv
-
 from database.db_handler import DBHandler
 from recipe_api.recipe_api_handler import RecipeAPIHandler
 
 
 def main():
-    load_dotenv()
-    api_key = os.getenv("SPOONACULAR_API_KEY")
+    load_dotenv()  # Load environment variables from .env file
+
+    api_key = os.getenv(
+        "SPOONACULAR_API_KEY"
+    )  # Get Spoonacular API key from environment variable
     if not api_key:
         print(
             "Please set the SPOONACULAR_API_KEY environment variable in the .env file."
         )
         return
 
-    db_handler = DBHandler("ingredients.db")
-    recipe_handler = RecipeAPIHandler(api_key)
+    db_handler = DBHandler("ingredients.db")  # Instantiate a database handler
 
+    recipe_handler = RecipeAPIHandler(api_key)  # Instantiate a recipe API handler
+
+    # Add ingredients to the database
     db_handler.add_ingredient("Milk", "2023-06-10")
     db_handler.add_ingredient("Eggs", "2023-06-15")
 
-    ingredients = db_handler.get_ingredients()
+    ingredients = (
+        db_handler.get_ingredients()
+    )  # Fetch all ingredients from the database
 
-    recipes = recipe_handler.fetch_recipes(ingredients)
-    print(recipes)
+    recipes = recipe_handler.fetch_recipes(
+        ingredients
+    )  # Fetch recipes based on the ingredients
 
-    db_handler.close()
+    # Write the recipes to a text file
+    with open("recipes.txt", "w", encoding="utf-8") as file:
+        for recipe in recipes:
+            file.write(str(recipe) + "\n")
+
+    db_handler.close()  # Close the database connection
 
 
 if __name__ == "__main__":
-    main()
+    main()  # Run the main function if the script is run directly
