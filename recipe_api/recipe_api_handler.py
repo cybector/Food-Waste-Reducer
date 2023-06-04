0ab5579109f6ecf62857ffef987cf29e2409abb2
cybector <tylersphotography2018@gmail.com>
2023-06-04 08:03:05 -0400

    Improved URL construction and added timeout to API request in recipe_api_handler.py.

--- recipe_api/recipe_api_handler.py
+++ recipe_api/recipe_api_handler.py
@@ -3,22 +3,26 @@ import requests
 
 class RecipeAPIHandler:
     def __init__(self, api_key):
-        self.api_key = api_key
+        self.api_key = api_key  # Store the API key
 
     def fetch_recipes(self, ingredients):
         # Create a comma-separated list of ingredients
         ingredient_list = ",".join([ingredient[0] for ingredient in ingredients])
 
-        # Make a request to the API
-        response = requests.get(
-            f"https://api.spoonacular.com/recipes/findByIngredients?ingredients={ingredient_list}&apiKey={self.api_key}"
-        )
+        # API endpoint
+        api_endpoint = "https://api.spoonacular.com/recipes/findByIngredients"
+
+        # Prepare URL for the API request
+        url = f"{api_endpoint}?ingredients={ingredient_list}&apiKey={self.api_key}"
+
+        # Make a request to the API with a 5-second timeout
+        response = requests.get(url, timeout=5)
 
         # If the request failed, print the status code and return an empty list
         if response.status_code != 200:
             print(f"Failed to fetch recipes: {response.status_code}")
             return []
 
-        # Return the list of recipes
+        # Parse the JSON response and return the list of recipes
         recipes = response.json()
         return recipes
